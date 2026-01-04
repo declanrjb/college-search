b5646a274fc921ffe269d059b1576eef38d5599a
declanrjb <declanrjb@gmail.com>
2026-01-04 16:48:10 -0500

    gender and ethnicity data

--- scripts/enroll-cleanup.R
+++ scripts/enroll-cleanup.R
@@ -1,3 +1,4 @@
+library(plyr)
 library(tidyverse)
 source('scripts/functions.R')
 
@@ -7,12 +8,12 @@ read_enroll_file <- function(file) {
   
   colnames(temp) <- colnames(temp) |>
     str_to_upper()
-  
-  temp <- temp |>
-    select(UNITID, YEAR, EFFYLEV, ends_with('T')) |>
-    select(!starts_with('X'))
 
-  if (dim(temp)[2] == 14) {
+  temp <- temp |>
+      select(UNITID, YEAR, EFFYLEV, EFYTOTLM, EFYTOTLW, ends_with('T')) |>
+      select(!starts_with('X'))
+
+  if (dim(temp)[2] <= 17) {
     return(temp)
   }
 }
@@ -21,16 +22,54 @@ enroll_files <- list.files('data/ipeds-clean/enrollment', full.names=TRUE)
 
 df <- enroll_files |>
   lapply(read_enroll_file) %>%
-  do.call(rbind, .)
+  do.call(rbind.fill, .) |>
+  mutate(YEAR = parse_number(YEAR))
 
 # select subgroup all students total
-df <- df |>
-  filter(EFFYLEV == 1) |>
-  select(!EFFYLEV)
+grad_tables <- df |>
+  select(UNITID, YEAR, EFFYLEV, EFYTOTLT) |>
+  mutate(EFFYLEV = case_when(
+    EFFYLEV == 1 ~ 'total',
+    EFFYLEV == 2 ~ 'undergraduate',
+    EFFYLEV == 4 ~ 'graduate'
+  )) |>
+  filter(!is.na(EFFYLEV)) |>
+  pivot_wider(id_cols=c('UNITID', 'YEAR'), names_from='EFFYLEV', values_from='EFYTOTLT') |>
+  mutate(
+    graduate = replace_na(graduate, 0),
+    undergraduate = replace_na(undergraduate, 0)
+  )
+
+# NEED TO CHECK THESE YEARS LINE UP
+adm_data <- read_csv('data/admissions.csv')
+
+adm_data <- adm_data |>
+  select(Year, UNITID, Enrolled) |>
+  rename(first_year = Enrolled)
+
+grad_tables <- grad_tables |>
+  left_join(adm_data, by=c('YEAR' = 'Year', 'UNITID'))
+
+# weird that there are 45 colleges with more first years than total students, check that
+grad_tables |>
+  mutate(
+    grad_undergrad_add_up = total == undergraduate + graduate,
+    first_years_under_total = first_year <= total
+  ) |>
+  count(grad_undergrad_add_up, first_years_under_total)
+
+grad_tables |>
+  write.csv('data/enrollment.csv', row.names=FALSE)
+
+
+
 
 enr_codebook <- read_csv('data/vars/effy_vars.csv')
 
-df <- df |>
+
+demographics <- df |>
+  filter(EFFYLEV == 1) |>
+  select(!EFFYLEV) |>
   pivot_longer(cols=starts_with('E'), names_to='demo') |>
   left_join(
     enr_codebook |>
@@ -40,18 +79,17 @@ df <- df |>
   select(!demo) |>
   rename(demo = varTitle)
 
-df <- df |>
+demographics <- demographics |>
   rename(students = value)
 
-total_students <- df |>
-  filter(demo == 'Grand total') |>
-  select(!demo) |>
-  arrange(UNITID, desc(YEAR)) |>
-  rename(`Total Students` = students)
+message(max(demographics$YEAR))
 
-demographics <- df |>
-  filter(YEAR == 2024) |>
-  select(!YEAR) |>
+# only focus on most recent year
+demographics <- demographics |>
+  filter(YEAR == max(YEAR)) |>
+  select(!YEAR)
+
+demographics <- demographics |>
   arrange(UNITID) |>
   filter(demo != 'Grand total') |>
   rename(
@@ -59,8 +97,37 @@ demographics <- df |>
     Demographic = demo
   )
 
-total_students |>
-  write.csv('data/enroll-total.csv', row.names=FALSE)
+gender <- demographics |>
+  filter(Demographic %in% c('Grand total men', 'Grand total women')) |>
+  mutate(
+    Demographic = Demographic |>
+      str_replace('Grand total', '') |>
+      str_replace('Total of gender unknown and another gender', 'unknown/other') |>
+      str_squish()
+  )
+  
+gender |>
+  write.csv('data/gender.csv', row.names=FALSE)
 
-demographics |>
-  write.csv('data/demographics.csv', row.names=FALSE)
+# ethnicity
+ethnicity <- demographics |>
+  filter(!(Demographic %in% c("Grand total men", "Grand total women"))) |>
+  filter(Demographic != "Total of gender unknown and another gender") |>
+  mutate(
+    Demographic = Demographic |>
+      str_replace('total', '') |>
+      str_squish()
+  ) |>
+  filter(Demographic %in% c(
+    "American Indian or Alaska Native",
+    "Asian",
+    "Black or African American",
+    "Hispanic or Latino",
+    "Native Hawaiian or Other Pacific Islander",
+    "White",
+    "Two or more races",
+    "Race/ethnicity unknown"
+  ))
+
+ethnicity |>
+  write.csv('data/ethnicity.csv', row.names=FALSE)
