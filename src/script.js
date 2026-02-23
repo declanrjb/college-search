8ca3e05ebdbd1a44c6d4f59b01011022332fee6b
declanrjb <declanrjb@gmail.com>
2026-02-22 20:45:43 -0500

    change request src

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 import Chart from 'chart.js/auto'
 
-var request_stem = 'http://127.0.0.1:5000'
-// var request_stem = 'https://college-search.onrender.com'
+// var request_stem = 'http://127.0.0.1:5000'
+var request_stem = 'https://college-search.onrender.com'
 var chart_types = {
     'propublica': 'bar',
     'admissions': 'line',
